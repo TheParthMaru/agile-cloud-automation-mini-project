56360a2994adb079aa2f42f5e3d3452a4c610104
Parth Maru <theparthmaru@gmail.com>
2024-11-04 15:21:22 +0000

    Added Exercise_02 questions

---
+++ src/main/groovy/grp23/Exercise_02.groovy
@@ -0,0 +1,11 @@
+package grp23
+
+// Use StudentsPerformance.json file as a dataset
+// Create query for each Selection, Projection, Filtering and Grouping
+// Use groovy collections to perform the same things as you would be able to do with mongodb.
+
+/**
+ * Steps:
+ * 1. Load the json dataset
+ * 2. Implement your query in groovy (Using collection methods like find(), collect(), groupBy(), etc) 
+ */
